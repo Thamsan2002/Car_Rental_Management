04a2b98acfcce76f0b93e4f3ea631fc351bbe275
Shankavi23 <shankavishankavi649@gmail.com>
2025-09-02 21:06:43 +0530

    payment managment form

---
+++ Car_Rental_Management/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Rental_Management.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Paymentdetails()
+        {
+            return View();
+        }
+    }
+}
