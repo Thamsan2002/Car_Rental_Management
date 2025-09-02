8b14b7cfd80d8eaa837b4a3cf193fa79c06bd71c
Shankavi23 <shankavishankavi649@gmail.com>
2025-09-02 16:09:35 +0530

    bookings form page

---
+++ Car_Rental_Management/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Rental_Management.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Booking()
+        {
+            return View();
+        }
+    }
+}
