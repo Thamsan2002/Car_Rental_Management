97534c4ba01bce6c87b160777b6ade6610360e7d
SaIf-ahmed05 <musadeensaif507@gmail.com>
2025-09-09 13:22:38 +0530

    STAFF

--- Car_Rental_Management/Repository/Implement/UserRepository.cs
+++ Car_Rental_Management/Repository/Implement/UserRepository.cs
@@ -13,17 +13,33 @@ namespace Car_Rental_Management.Repository.Implement
         {
             _context = context;
         }
-        public async Task<User> AddAsync(User user)
+        public async Task<Guid> AddAsync(User user)
         {
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return user;
+            return user.Id;
+        }
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<User> GetByEmailAsync(string email)
         {
             return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
         }
+        public async Task<User?> GetByPhoneAsync(string phoneNumber)
+        {
+            return await _context.Users
+                                 .FirstOrDefaultAsync(u => u.PhoneNumber == phoneNumber);
+        }
+
 
 
     }
