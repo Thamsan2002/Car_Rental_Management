d15c59b85c09da80ba1dd5c508b6a26bb3ef359b
Thamsan2002 <plthamsan02@gmail.com>
2025-09-12 12:26:21 +0530

    clear error

--- Car_Rental_Management/Controllers/AddCarController.cs
+++ Car_Rental_Management/Controllers/AddCarController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Car_Rental_Management.Service.Interface;
+using Car_Rental_Management.ViewModel;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Car_Rental_Management.Controllers
 {
     public class AddCarController : Controller
     {
+        private readonly ICarService _carService;
+
+        public AddCarController(ICarService carService)
+        {
+            _carService = carService;
+        }
         public IActionResult Details()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult AddCar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCar(CarViewModel carViewModel)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return View(carViewModel); // Re-render form with validation errors
+            //}
+
+            var result = await _carService.AddCarAsync(carViewModel);
+
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Car added successfully!";
+                return RedirectToAction("CarList"); // or wherever you want
+            }
+            else
+            {
+                ModelState.AddModelError("", "Something went wrong while saving the car.");
+                return View(carViewModel);
+            }
         }
     }
 }
---
+++ Car_Rental_Management/Service/Interface/ICarService.cs
@@ -0,0 +1,10 @@
+﻿using Car_Rental_Management.ViewModel;
+using System.Threading.Tasks;
+
+namespace Car_Rental_Management.Service.Interface
+{
+    public interface ICarService
+    {
+        Task<bool> AddCarAsync(CarViewModel model);
+    }
+}
