66ef2cd2084810fc610c00b1a4a7b65434232d01
thifshana <thivithivy@gmail.com>
2025-09-16 14:41:34 +0530

    driver delete

--- Car_Rental_Management/Service/Interface/IDriverService.cs
+++ Car_Rental_Management/Service/Interface/IDriverService.cs
@@ -1,14 +1,16 @@
 ﻿using Car_Rental_Management.Dtos;
 using Car_Rental_Management.Models;
 using Car_Rental_Management.ViewModel;
+using System.Threading.Tasks;
 
 namespace Car_Rental_Management.Service.Interface
 {
     public interface IDriverService
     {
         Task<string> CreateDriverAsync(DriverViewModel viewModel);
-        Task<DriverDto> GetDriverByIdAsync(Guid id);
-        Task<IEnumerable<DriverDto>> GetAllDriversAsync();
-        Task UpdateDriverAsync(DriverViewModel viewModel);
+        Task<List<DriverDto>> GetAllDriversAsync();
+        Task<DriverDto?> GetDriverByIdAsync(Guid id);
+        Task<string> UpdateDriverAsync(DriverViewModel viewModel);
+        Task<string> DeleteDriverAsync(Guid id);
     }
 }
