45cd0e5b801e7ecd6a9fb15165951812a23d9b88
Shankavi23 <shankavishankavi649@gmail.com>
2025-09-16 15:24:41 +0530

    finished customer

--- Car_Rental_Management/Repository/Interface/IUserRepository.cs
+++ Car_Rental_Management/Repository/Interface/IUserRepository.cs
@@ -6,12 +6,10 @@ namespace Car_Rental_Management.Repository.Interface
     {
 
         Task<Guid> AddAsync(User user);
-        //Task<User?> GetByIdAsync(Guid id);
-        //Task UpdateAsync(User user);
+        Task<User?> GetByIdAsync(Guid id);
+        Task UpdateAsync(User user);
         Task<bool> IsEmailOrPhoneExistAsync(string email, string phone);
-        //Task<User> GetByEmailAndPhoneAsync(string email, string phone);
-        Task<User?> GetByEmailOrPhoneAsync(string emailOrPhone, string password);
-
+        Task<User?> GetByPhoneAsync(string phoneNumber);
         Task<User?> GetCustomerByLoginAsync(string emailOrPhone, string password);
 
 
