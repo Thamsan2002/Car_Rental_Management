8df4fc29c1607c06547ece78e275921ea21e44c7
Thamsan2002 <plthamsan02@gmail.com>
2025-09-20 22:39:19 +0530

    finished booking

--- Car_Rental_Management/Program.cs
+++ Car_Rental_Management/Program.cs
@@ -59,6 +59,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=AdminLogin}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
